93d9c435d711ec4538b6a62932bc9b4529364476
yoshioka-s <yoshioka63@gmail.com>
2015-12-18 00:05:45 -0800

    [add] onChange event on the input

---
+++ scripts/components/NameInput.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+/*
+ * input component for new item name
+ *
+ */
+var NameInput = React.createClass({
+  getInitialState: function () {
+    return {name: ''};
+  },
+  handleNameChange: function (e) {
+    console.log('onChange!');
+    this.setState({name: e.target.value});
+  },
+
+  render: function() {
+    return (
+      <div className="name-input">
+        <input
+          type="text"
+          placeholder="ENTER ITEM"
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+      </div>
+    );
+  }
+});
+module.exports = NameInput;
